3a2ba0798678116af1bc40ec4fc850ce9c1758dc
Shutak0 <111364245+Shutak0@users.noreply.github.com>
2022-08-23 15:34:56 +0200

    2 files

---
+++ CRub.py
@@ -0,0 +1,42 @@
+from random import randint
+class Unit:
+    def __init__(self, vid, name, damage, health, speed, armorlvl):
+        self.vid = vid
+        self.name = name
+        self.damage = damage
+        self.health = health
+        self.speed = speed
+        self.armorlvl = armorlvl
+    def hitenemy(self, aname):
+        hit = 0
+        if self.vid == 'Magic':  #crit chance
+            crit = randint(1, 10)
+        elif self.vid == 'Voin':
+            crit = randint(1, 7)
+        elif self.vid == 'Strelok':
+            crit = randint(1, 9)
+        elif self.vid == 'Komandir':
+            crit = randint(1, 6) 
+        if crit <= 2:
+            hit = hit + (crit*10)
+        ukloneniechance = (aname.speed*10 - aname.armorlvl/2) * randint(0, 5) #evasion chance
+        misschance = randint(0, 10) + aname.armorlvl #miss chance
+        hit = hit + self.damage - aname.armorlvl
+        if misschance > 15:
+            hit = 0
+        if ukloneniechance >= 50:
+            hit = 0
+        aname.health = aname.health - hit
+        print(self.name, ' did the hit. Current HP: ', self.health, ' Enemy HP: ', aname.health)
+class Magic(Unit):
+    def __init__(self, name, damage, health, speed, armorlvl):
+        super().__init__(vid='Magic', name=name, damage=damage, health=health, speed=speed, armorlvl=armorlvl)
+class Voin(Unit):
+    def __init__(self, name, damage, health, speed, armorlvl):
+        super().__init__(vid='Voin', name=name, damage=damage, health=health, speed=speed, armorlvl=armorlvl)
+class Strelok(Unit):
+    def __init__(self, name, damage, health, speed, armorlvl):
+        super().__init__(vid='Strelok', name=name, damage=damage, health=health, speed=speed, armorlvl=armorlvl)
+class Komandir(Unit):
+    def __init__(self, name, damage, health, speed, armorlvl):
+        super().__init__(vid='Komandir', name=name, damage=damage, health=health, speed=speed, armorlvl=armorlvl)
--- Rub.py
+++ Rub.py
@@ -2,47 +2,10 @@ from ast import Global
 from random import random
 from random import randint
 from tkinter import *
-class Unit:
-    def __init__(self, vid, name, damage, health, speed, armorlvl):
-        self.vid = vid
-        self.name = name
-        self.damage = damage
-        self.health = health
-        self.speed = speed
-        self.armorlvl = armorlvl
-    def hitenemy(self, aname):
-        hit = 0
-        if self.vid == 'Magic':  #crit chance
-            crit = randint(1, 10)
-        elif self.vid == 'Voin':
-            crit = randint(1, 7)
-        elif self.vid == 'Strelok':
-            crit = randint(1, 9)
-        elif self.vid == 'Komandir':
-            crit = randint(1, 6) 
-        if crit <= 2:
-            hit = hit + (crit*10)
-        ukloneniechance = (aname.speed*10 - aname.armorlvl/2) * randint(0, 5) #evasion chance
-        misschance = randint(0, 10) + aname.armorlvl #miss chance
-        hit = hit + self.damage - aname.armorlvl
-        if misschance > 15:
-            hit = 0
-        if ukloneniechance >= 50:
-            hit = 0
-        aname.health = aname.health - hit
-        print(self.name, ' did the hit. Current HP: ', self.health, ' Enemy HP: ', aname.health)
-class Magic(Unit):
-    def __init__(self, name, damage, health, speed, armorlvl):
-        super().__init__(vid='Magic', name=name, damage=damage, health=health, speed=speed, armorlvl=armorlvl)
-class Voin(Unit):
-    def __init__(self, name, damage, health, speed, armorlvl):
-        super().__init__(vid='Voin', name=name, damage=damage, health=health, speed=speed, armorlvl=armorlvl)
-class Strelok(Unit):
-    def __init__(self, name, damage, health, speed, armorlvl):
-        super().__init__(vid='Strelok', name=name, damage=damage, health=health, speed=speed, armorlvl=armorlvl)
-class Komandir(Unit):
-    def __init__(self, name, damage, health, speed, armorlvl):
-        super().__init__(vid='Komandir', name=name, damage=damage, health=health, speed=speed, armorlvl=armorlvl)
+from CRub import Voin
+from CRub import Komandir
+from CRub import Strelok
+from CRub import Magic
 def fstart(dude0, dude1):
     global winner
     global loser
@@ -120,7 +83,7 @@ def startf():
     fstart(guy0, guy1)
     lbl5 = Label(window, text=("Победитель - ", winner, ", Проигравший - ", loser, ". Жизней у победителя - ", whealth, ", Жизней у проигравшего - ", lhealth), font=("Arial", 10))
     lbl5.grid(column=2, row = 4)
-btn3 = Button(window, width=15, text="Начать резню!", command=startf)
+btn3 = Button(window, width=15, text="Начать бой!", command=startf)
 btn3.grid(column=2, row=3)
 
 window.mainloop()
